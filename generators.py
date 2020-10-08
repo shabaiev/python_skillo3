8ae4605e0785bce63feed4e415dd6401bc487c48
shabaiev <ilyashabaev2209@gmail.com>
2020-10-08 14:55:27 -0500

    generators file was added

---
+++ generators.py
@@ -0,0 +1,11 @@
+def square_nums(nums):
+    for i in nums:
+        while i > 0:
+            yield i * i
+            i += 1
+
+
+my_nums = square_nums([1, 2, 9, 16, 25, 36])
+
+print(next(my_nums))
+
