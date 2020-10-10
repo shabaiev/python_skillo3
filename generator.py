4206722157a688081281facdcbde4aa3be90cd40
shabaiev <ilyashabaev2209@gmail.com>
2020-10-10 10:17:14 -0500

    adjusted solution

---
+++ generator.py
@@ -0,0 +1,31 @@
+def square_nums(nums):
+    for i in nums:
+        while i > 0:
+            yield i * i
+            i += 1
+
+
+my_nums = square_nums([1, 2, 9, 16, 25, 36])
+
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
+print(next(my_nums))
